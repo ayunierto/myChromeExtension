c955e3b5a139f0612265ff085551563908823683
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-11-14 04:01:59 -0500

    add tags <a> and <code> for notranslate

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ function addNotranslateClassToPre() {
     var preElement = preElements[i];
     preElement.classList.add("notranslate");
   }
+
+  var aElements = document.getElementsByTagName("a");
+
+  for (var i = 0; i < aElements.length; i++) {
+    var preElement = aElements[i];
+    preElement.classList.add("notranslate");
+  }
+
+  var codeElements = document.getElementsByTagName("code");
+
+  for (var i = 0; i < codeElements.length; i++) {
+    var preElement = codeElements[i];
+    preElement.classList.add("notranslate");
+  }
 }
 
 addNotranslateClassToPre();
